8d2fc8b83a2eaab5754a0c13da5f7bf6e58bba79
Norbert Gocze <norbert.gocze@evotrex.eu>
2022-02-18 10:30:51 +0100

    play with global object

---
+++ global_object/app.js
@@ -0,0 +1,15 @@
+// js
+
+// setTimeout()
+// clearTimeout()
+
+// setInterval()
+// clearInterval()
+
+// node
+// window doesn't exsist in nodejs runtime.
+
+// variables and funtions are not added to the global object, unlike window
+var message = '';
+global.console.log("hi");
+console.log(global.message);
